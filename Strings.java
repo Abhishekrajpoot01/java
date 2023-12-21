30c10d5d6225314dc14a9c017ae5eca410b2f773
Abhishekrajpoot01 <153645262+Abhishekrajpoot01@users.noreply.github.com>
2023-12-21 18:44:21 +0530

    comparing string

---
+++ Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+    public static void main(String[] args) {
+      String name1 = "Abhishek";
+      String name2 = "Abhishek";
+      if(name1.compareTo(name2)== 0){
+        System.out.println("Strings are equal ");
+      }
+      else{
+        System.out.println("Strings are not equal");
+      }
+    }
+}
