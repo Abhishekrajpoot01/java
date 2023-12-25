95cfa48b9571319cb399d6a70d74664a976d4ca9
Abhishekrajpoot01 <153645262+Abhishekrajpoot01@users.noreply.github.com>
2023-12-25 18:42:43 +0530

    Array list program in java

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,16 @@
 import java.util.*;
 public class Recursion {
-    public static void printSum(int i,int n,int sum){
-        if(i==n){
-            sum = sum +i;
-            System.out.println(sum);
-            return;
+    public static void printSum(int n){
+        if(n==0){
+            return ;
         }
-        sum = sum + i;
-        printSum(i+1,n,sum);
+        printSum(n-1);
+        System.out.println(n);
     }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the number ");
         int n= sc.nextInt();
-        printSum(1,n,0);
+        printSum(n);
     }
 }
